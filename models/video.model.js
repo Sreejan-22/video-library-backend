720ed44c20a8807912f621bcd4886d37a0196c72
Sreejan <sreejan147@gmail.com>
2021-11-14 19:58:10 +0530

    feat: add timestamps property to video model

--- models/video.model.js
+++ models/video.model.js
@@ -1,30 +1,33 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const videoSchema = new Schema({
-  // 3 categories - highlights, tutorial, performances
-  category: {
-    type: String,
-    required: true,
+const videoSchema = new Schema(
+  {
+    // 3 categories - highlights, tutorial, performances
+    category: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    channel: {
+      type: String,
+      required: true,
+    },
   },
-  url: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  channel: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Video = mongoose.model("video", videoSchema);
 module.exports = Video;
